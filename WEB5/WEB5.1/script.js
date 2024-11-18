52efb3e4c48ed5e1b898c1d866f4bb99cea1209f
shezanutiy <victorchc2005@gmail.com>
2024-11-18 19:02:15 +0700

    Update script.js

--- WEB5/WEB5.1/script.js
+++ WEB5/WEB5.1/script.js
@@ -1,8 +1,8 @@
 document.getElementById('orderForm').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    const day = parseInt(document.getElementById('day').value); // День недели (1-7)
-    const time = parseInt(document.getElementById('time').value); // Время заказа (0-23)
+    const day = parseInt(document.getElementById('day').value); 
+    const time = parseInt(document.getElementById('time').value); 
     const result = document.getElementById('result');
 
     if (day === 7) {
@@ -16,7 +16,7 @@ document.getElementById('orderForm').addEventListener('submit', function(event) 
     if (time >= 8 && time < 16) {
         deliveryTime = "после 16.00";
     } else if (time >= 16) {
-        deliveryDay = day === 6 ? 1 : day + 1; // Переход на следующий день или понедельник
+        deliveryDay = day === 6 ? 1 : day + 1; 
     }
 
     const days = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];

59d3e73650b0f7c652df94cbb25e4019a16f38b7
shezanutiy <victorchc2005@gmail.com>
2024-12-08 18:24:15 +0700

    Add files via upload

--- WEB8/WEB8.1/web8a.php
+++ WEB8/WEB8.1/web8a.php
@@ -1,1 +1,45 @@
-1
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Таблица умножения</title>
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 50%;
+            margin: 20px auto;
+        }
+        th, td {
+            border: 1px solid #000;
+            text-align: center;
+            padding: 5px;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+
+<table>
+    <thead>
+        <tr>
+            <th>x</th>
+            <?php for ($i = 0; $i <= 10; $i++): ?>
+                <th><?= $i ?></th>
+            <?php endfor; ?>
+        </tr>
+    </thead>
+    <tbody>
+        <?php for ($i = 0; $i <= 10; $i++): ?>
+            <tr>
+                <th><?= $i ?></th>
+                <?php for ($j = 0; $j <= 10; $j++): ?>
+                    <td><?= $i * $j ?></td>
+                <?php endfor; ?>
+            </tr>
+        <?php endfor; ?>
+    </tbody>
+</table>
+
+</body>
+</html>
